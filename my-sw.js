7db9edbf148b69f9a133fda80489b03ee7ff9b32
Ryosuke Endo <ryosuke5000mekiden@gmail.com>
2021-01-24 22:55:46 +0900

    restore

--- my-sw.js
+++ my-sw.js
@@ -1,28 +1,52 @@
-var CACHE_NAME = 'Muscle-memo-caches';
-var urlsToCache = [
-    'index.html',
-    'style.css',
-    'main.js'
+const cacheName = "v1";
+
+const cacheAssets = [
+    "index.html",
+    "style.css",
+    "main.js"
 ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+
+
+self.addEventListener("install", async (ev) => {
+    console.log("SW: install eventが発火");
+    ev.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        cache.addAll(cacheAssets);
+        return self.skipWaiting();
+    }) ()
+
     );
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
+self.addEventListener("activate", async (ev) => {
+    console.log("SW: activate eventが発火");
+    ev.waitUntil((async () => {
+      const keys = await caches.keys();
+      console.log(keys);
+      const targets = keys.filter(key => key !== cacheName);
+      console.log(targets)
+      return Promise.all(targets.map(target => caches.delete(target)));
+    }) ()
+
     );
+});
+
+self.addEventListener("fetch", async (ev) => {
+    ev.respondWith((async () => {
+        const hit = await caches.match(ev.request);
+        if(hit) {
+          return hit;
+        }
+
+        try {
+          const res = await fetch(ev.request);
+          const resClone = res.clone();
+          const cache = await caches.open(cacheName);
+          cache.put(ev.request, resClone);
+          return res;
+        } catch(error) {
+          return new Response(error);
+        }
+    }) ());
 });
